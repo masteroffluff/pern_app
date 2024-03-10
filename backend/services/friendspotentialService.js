97cf9078168e167cbdace0a57ab1040ec0ee2dea
masteroffluff <chris.chapman@linuxmail.org>
2024-03-10 20:02:31 +0000

    potential friends done

--- backend/services/friendspotentialService.js
+++ backend/services/friendspotentialService.js
@@ -1,3 +1,4 @@
+const e = require('express');
 const db = require('../utils/db')
 
 module.exports.funcfriendspotential = function funcfriendspotential(req, res) {
@@ -7,20 +8,25 @@ module.exports.funcfriendspotential = function funcfriendspotential(req, res) {
 }
 
 module.exports.get_potentialFriends = async function get_potentialFriends(req, res) {
+    try {
+        const { id } = req.user
 
-    const {id} = req.user
+        const potentialFriends = await db.queryPromisified(
+            `SELECT id, display_name
+            FROM "Users"
+            WHERE id <> $1
+                AND NOT EXISTS (
+                    SELECT 1
+                    FROM "Friends"
+                    WHERE (user_id = $1 AND friend_id = "Users".id)
+                    OR (user_id = "Users".id AND friend_id = $1)
+        );`, [id])
 
-    const friendsOfFriends = await db.query(
-        `SELECT id, display_name 
-        FROM "Users"
-        JOIN "Friends" ON "Users".id = `)
 
-
-    
-    
-
-    res.send({
-        message: 'This is the mockup controller for get_potentialFriends'
-    });
+        return res.send(potentialFriends.rows);
+    }catch(e){
+        console.log(e)
+        return res.status(400).send({message:e.message})
+    }
 }
 
