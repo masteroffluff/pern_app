ea21f1825609185b52b6b20b6ea0d00b84ae2d20
masteroffluff <chris.chapman@linuxmail.org>
2024-03-20 21:47:33 +0000

    calendar primitive

--- src/components/mainPage/subitems/DisplayCalendar.js
+++ src/components/mainPage/subitems/DisplayCalendar.js
@@ -1,42 +1,109 @@
-import React,{ useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import moment from "moment";
 import { useDispatch, useSelector } from "react-redux";
 import { calendarFetch, selectCalendar } from "../../calandar/calendarSlice";
 import { useNavigate } from "react-router";
+
 import DisplayItem from "../DisplayItem";
+
+function checkNested(obj, props) {
+    return props.reduce((accumulator, prop) => {
+        if (accumulator && accumulator.hasOwnProperty(prop)) {
+            return accumulator[prop];
+        }
+        return undefined;
+    }, obj);
+}
 
 export default function DisplayCalendar() {
     const navigate = useNavigate()
     const dispatch = useDispatch()
-    var d = new Date();
-    const [dateFrom, setDateFrom] = useState(d)
-    const [dateTo, setDateTo] = useState(d.setMonth(d.getMonth() - 1))
 
+    var dFrom = moment().add(-1, 'M');
+    //dFrom.setMonth(dFrom.getMonth() - 1)   
+    const [dateFrom, setDateFrom] = useState(dFrom)
+    var dTo = moment().add(1, 'M');
+    //dTo.setMonth(dFrom.getMonth() + 1)   
+    const [dateTo, setDateTo] = useState(dTo)
 
-    useEffect(()=>{
+    const [calendarMap, setCalendarMap] = useState([])
+    const calendar = useSelector(selectCalendar)
+
+    useEffect(() => {
         dispatch(calendarFetch())
     }, [dispatch]);
 
-    const dateFrom_change=(e)=>{
+    useEffect(() => {
+
+        const mapCalendar = (_dateFrom, _dateTo) => {
+
+            // why god why??
+            //console.log(_dateFrom, _dateTo)
+            const _calendarMap = {}
+            let currentDate = moment(_dateFrom)
+            while (currentDate.isSameOrBefore(_dateTo)) {
+                //console.log(currentDate)
+                const year = currentDate.format('YYYY');
+                const month = currentDate.format('MM');
+                const day = currentDate.format('DD');
+
+                if (!_calendarMap[year]) {
+                    _calendarMap[year] = {};
+                }
+                if (!_calendarMap[year][month]) {
+                    _calendarMap[year][month] = {};
+                }
+                if (!_calendarMap[year][month][day]) {
+                    _calendarMap[year][month][day] = [];
+                }
+
+                currentDate = currentDate.add(1, 'day');
+            }
+
+            calendar.forEach((item) => {
+                // const day = item.date_from.getDate()
+                // const year = item.date_from.getFullYear()
+                // const month = item.date_from.getMonth()
+                const itemDT = moment(item.date_from)
+                //console.log(itemDT)
+                var day = itemDT.format('DD');
+                var month = itemDT.format('MM');
+                var year = itemDT.format('YYYY');
+                //console.log(item, [year,month,day])
+                if (checkNested(_calendarMap, [year, month, day])) {
+
+                    _calendarMap[year][month][day].push(item)
+                }
+            }
+
+            )
+            console.log('calendarmap', _calendarMap)
+            return _calendarMap
+        }
+        if (calendar.length > 0) { setCalendarMap(mapCalendar(dateFrom, dateTo)) }
+    }, [calendar, dateFrom, dateTo]);
+
+    const dateFrom_change = (e) => {
         e.preventDefault()
-        setDateFrom(e.target.value)
+        setDateFrom(new Date(e.target.value))
     }
-    const dateTo_change=(e)=>{
+    const dateTo_change = (e) => {
         e.preventDefault()
-        setDateTo(e.target.value)
+        setDateTo(new Date(e.target.value))
     }
 
-    const calendar = useSelector(selectCalendar)
 
-    const newEvent_click = (e) =>{
+
+    const newEvent_click = (e) => {
         e.preventDefault()
         navigate('/newevent')
     }
-    const newReminder_click = (e) =>{
+    const newReminder_click = (e) => {
         e.preventDefault()
         navigate('/newreminder')
     }
 
-    const newAppointment_click = (e) =>{
+    const newAppointment_click = (e) => {
         e.preventDefault()
         navigate('/newappointment')
     }
@@ -45,18 +112,34 @@ export default function DisplayCalendar() {
 
     return <div data-testid="displayCalendar">
         <h3>Calendar</h3>
+        {/* <p>...{JSON.stringify(calendarMap)}</p> */}
         <form>
             <label htmlFor="dateFrom">Date From</label>
-            <input data-testid="dateFrom" type='date' id='dateFrom' value={dateFrom} onChange={dateFrom_change} />
+            <input data-testid="dateFrom" type='date' id='dateFrom' value={dateFrom.format('YYYY-MM-DD')} onChange={dateFrom_change} />
 
             <label htmlFor="dateTo">Date To</label>
-            <input data-testid="dateTo" type='date' id='dateTo' value={dateTo} onChange={dateTo_change}/>
+            <input data-testid="dateTo" type='date' id='dateTo' value={dateTo.format('YYYY-MM-DD')} onChange={dateTo_change} />
         </form>
-        <ul>
-            {calendar.map((e,i)=><li key={i}><DisplayItem data={e}/></li>)}
-        </ul>
+        {Object.entries(calendarMap).map(([yr, mn], i) => {
+            return (<><h4 key={i}>{yr}</h4>
+                {Object.entries(mn).map(([mn_, dy], i) => {
+                    return (<>
+                        <h5 key={i}>{mn_}</h5>
+                        {
+                            Object.entries(dy).map(([dy_, items], i)=>{
+                                return (<><p>{dy_}</p>
+                                    {items.map((item)=><p>{item.title}</p>)}
+                                </>)
+                            }
+                        )}
+                    </>)
+                })}
+            </>)}
+        )}
+         
+
         <button data-testid='newEvent' value='newEvent' onClick={newEvent_click} >New Event</button>
-        <button data-testid='newReminder' value='newReminder' onClick={newReminder_click} >New Reminder</button>
-        <button data-testid='newAppointment' value='newAppointment' onClick={newAppointment_click} >New Appointment</button>
+            <button data-testid='newReminder' value='newReminder' onClick={newReminder_click} >New Reminder</button>
+            <button data-testid='newAppointment' value='newAppointment' onClick={newAppointment_click} >New Appointment</button>
     </div>
 }
