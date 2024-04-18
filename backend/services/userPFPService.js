04381640f4345d199ecee13c66bbf7c58a35f265
masteroffluff <chris.chapman@linuxmail.org>
2024-04-18 09:43:24 +0100

    updated pfp service to accept more types of image

--- backend/services/userPFPService.js
+++ backend/services/userPFPService.js
@@ -12,11 +12,11 @@ module.exports.funcuserpfp = function funcuserpfp(req, res) {
 }
 
 module.exports.get_user_pfp = async function get_user_pfp(req, res) {
-  let id = req.query.id==='0'?req.user.id:req.query.id
+  let id = req.query.id === '0' ? req.user.id : req.query.id
   console.log(`Looking for image id=${id} user id is ${req.user.id}`)
   try {
 
-    const callback = (imageData) => {
+    const callback = (imageData, ext) => {
       try {
 
         if (!imageData) {
@@ -24,16 +24,32 @@ module.exports.get_user_pfp = async function get_user_pfp(req, res) {
           err.status = 404
           throw err
         }
-        const { filename, data } = imageData;
+        const { ext, data } = imageData;
 
         // Set appropriate response headers
-        res.setHeader('Content-Disposition', `inline; filename=${filename}`);
-        res.setHeader('Content-Type', 'image/png');
+        let contentDisposition, contentType
+        switch (ext) {
+          case 'jpg':
+            contentDisposition=`inline; filename=PFP${id}.jpeg`
+            contentType= 'image/jpeg'
+            break
+          case 'png':
+            contentDisposition=`inline; filename=PFP${id}.png`
+            contentType= 'image/png'
+            break
+          case 'wbp':
+            contentDisposition=`inline; filename=PFP${id}.webp`
+            contentType= 'image/webp'
+            break
+          default:
+        }
+        res.setHeader('Content-Disposition', contentDisposition);
+        res.setHeader('Content-Type', contentType);
         // Send the image data
         console.log(data)
         res.send(data);
       } catch (error) {
-        throw(error)
+        throw (error)
       }
     }
     getImageById(id, callback);
@@ -47,8 +63,8 @@ module.exports.get_user_pfp = async function get_user_pfp(req, res) {
 async function getImageById(id, callback) {
   const at = new db.atomicTrasaction()
   try {
-    
-    const query = 'SELECT data FROM "User_PFP" WHERE id=$1;';
+
+    const query = 'SELECT data, ext FROM "User_PFP" WHERE id=$1;';
     await at.begin()
     const result = await at.query(query, [id]);
 
@@ -60,13 +76,13 @@ async function getImageById(id, callback) {
 
     // Extract filename and image data from the result
     //console.log(result.rows[0])
-    const { filename, data } = result.rows[0];
+    const { ext, data } = result.rows[0];
 
-    callback({ filename, data });
+    callback({ ext, data });
   } catch (error) {
     console.error('Error fetching image from the database:', error);
     throw error; // Rethrow the error for the calling code to handle
-  }finally{
+  } finally {
     at.releaseClient()
   }
 }
@@ -74,8 +90,8 @@ async function getImageById(id, callback) {
 
 module.exports.update_user_pfp = async function update_user_pfp(req, res) {
   try {
-    const sql_update = `UPDATE "User_PFP" set data = $2, ext = $3 FROM  WHERE id=$1 RETURNING data"`
-    const sql_insert = `INSERT INTO "User_PFP" (id, data) VALUES($1, $2) RETURNING data`
+    const sql_update = `UPDATE "User_PFP" set data = $2, ext = $3 FROM  WHERE id=$1 RETURNING data, ext"`
+    const sql_insert = `INSERT INTO "User_PFP" (id, data) VALUES($1, $2) RETURNING data, ext`
     const rowCount = await db.queryPromisified(`SELECT COUNT(*) as r FROM "User_PFP" WHERE id=$1"`, [req.user.id])
     let response;
     if (rowCount.rows[0].r > 0) {
@@ -83,12 +99,12 @@ module.exports.update_user_pfp = async function update_user_pfp(req, res) {
     } else {
       response = (await db.queryPromisified(sql_insert, [req.user.id, req.body.data, req.body.ext]));
     }
-    res.send(response.rows[0])
+    res.send(response.rows[0].data)
 
   } catch (e) {
     console.log('get_user_pfp error', e)
     return res.status(400).send(e)
   }
 
-}
+};
 
