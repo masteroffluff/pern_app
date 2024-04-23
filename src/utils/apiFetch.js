0700548949aa5d60e47bf5ab3b9d8237f6c33bfa
masteroffluff <chris.chapman@linuxmail.org>
2024-04-23 20:03:10 +0100

    readme spellcheck

--- src/utils/apiFetch.js
+++ src/utils/apiFetch.js
@@ -1,4 +1,4 @@
-const apiUrl = process.env.REACT_APP_API_URL
+//const apiUrl = process.env.REACT_APP_API_URL
 
 export default async function apiFetch(endPoint, options, rejectionCallback){
     try{
